b706363a24ce2470ff887421d59bdbf82a8c0864
guipleite <guipleite@gmail.com>
2017-03-31 08:01:30 -0300

    update main

---
+++ main.py
@@ -0,0 +1,15 @@
+arquivo = open("inspermons.json" , "r")
+import random
+
+while True:
+	op = str(input("Deseja Passear ou Dormir? "))
+	opl = op.lower()
+	if opl == "passear" :
+		
+		
+	if opl == "dormir" :
+		break
+		
+		
+	else:
+		print("Comando errado")
