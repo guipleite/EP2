2eedf5c146dca5e492cdda55c29b28be481914a9
guipleite <guipleite@gmail.com>
2017-03-31 08:33:22 -0300

    Tabs

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ arquivo = open("inspermons.json" , "r")
 import random
 
 def main(arquivo,random):
-while True:
-	op = str(input("Deseja Passear ou Dormir? "))
-	opl = op.lower()
-	if opl == "passear" :
-		ini = arquivo[random.int]
+	while True:
+		op = str(input("Deseja Passear ou Dormir? "))
+		opl = op.lower()
+		if opl == "passear" :
+			ini = arquivo[random.int]
 		
-	if opl == "dormir" :
-		break
+		if opl == "dormir" :
+			break
 		
 		
-	else:
-		print("Comando errado")
+		else:
+			print("Comando errado")
