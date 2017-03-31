068ba70808d9167973441e46a65ae3ee38f6dd1e
guipleite <guipleite@gmail.com>
2017-03-31 09:50:47 -0300

    varias coisas2

--- main.py
+++ main.py
@@ -1,41 +1,37 @@
 import random
 import json
-
-def mostra_ini(ipmon):
-	print("Inspermon : {0}".format(ipmon["nome"]))
 	
 def mostra_ipmon(ipmon):
+	
 	print("Inspermon : {0}".format(ipmon["nome"]))
 	print("poder = {0}".format(ipmon["poder"]))
 	print("vida = {0}".format(ipmon["vida"]))
 	print("defesa = {0}\n".format(ipmon["defesa"]))
 	
+print("Você pode encontrar esses inspermons por aí: \n")	
+
 with open('inspermons.json') as arquivo:
 		inspermons = json.load(arquivo)
 for ipmon in inspermons:
 		mostra_ipmon(ipmon)
 
-def main(arquivo,random):
-	while True:
-		op = str(input("Deseja Passear ou Dormir? "))
+while True:
+	op = str(input("Deseja Passear ou Dormir? \n"))
 		
-		if op.lower() == "passear" :
-			inte = random.randrange(len(arquivo))
-			print(arquivo[inte])
+	if op.lower() == "passear" :
+		inte = random.randrange(len(inspermons))
+		ini = inspermons[inte]
+		print("Seu inimigo é: {0}".format(ini["nome"]))
+			
 		
+	if op.lower() == "dormir" :
+		break
 		
-		if op.lower() == "dormir" :
-			break
-		
-		else:
-			print("Comando errado")
+	if op.lower()!="dormir" or op.lower() != "passear":
+		print("Comando errado")
 
 
 			
 
 			
 			
-			# with open('inspermons.json') as arquivo:
-				# inspermons = json.load(arquivo)
-			# for ipmon in inspermons:
-				# mostra_ini(ipmon)
