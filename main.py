b1f399511e714137976ab94337e63586445583ea
guipleite <guipleite@gmail.com>
2017-03-31 08:32:21 -0300

    :

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 arquivo = open("inspermons.json" , "r")
 import random
 
-def main(arquivo,random)
+def main(arquivo,random):
 while True:
 	op = str(input("Deseja Passear ou Dormir? "))
 	opl = op.lower()
