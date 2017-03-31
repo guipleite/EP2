cd4eb173c961708ef6e23556b2717769f0ac8bb5
guipleite <guipleite@gmail.com>
2017-03-31 09:12:57 -0300

    Escolhe um inimigo

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-arquivo = open("inspermons.json" , "r")
 import random
+import json
+
+def mostra_ini(ipmon):
+	print("Inspermon : {0}".format(ipmon["nome"]))
+	
+def mostra_ipmon(ipmon):
+	print("Inspermon : {0}".format(ipmon["nome"]))
+	print("poder = {0}".format(ipmon["poder"]))
+	print("vida = {0}".format(ipmon["vida"]))
+	print("defesa = {0}\n".format(ipmon["defesa"]))
+	
+with open('inspermons.json') as arquivo:
+		inspermons = json.load(arquivo)
+for ipmon in inspermons:
+		mostra_ipmon(ipmon)
 
 def main(arquivo,random):
 	while True:
 		op = str(input("Deseja Passear ou Dormir? "))
 		opl = op.lower()
 		if opl == "passear" :
-			ini = arquivo[random.int]
+			inte = random.randrange(len(d))
+			ini = arquivo[inte]
+			with open('inspermons.json') as arquivo:
+				inspermons = json.load(arquivo)
+			for ipmon in inspermons:
+				mostra_ini(ipmon)
 		
 		if opl == "dormir" :
 			break
@@ -14,3 +33,5 @@ def main(arquivo,random):
 		
 		else:
 			print("Comando errado")
+
+			
