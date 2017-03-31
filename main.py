7d088c3b115431bc53fd6e69656c9e4cddb80eb4
guipleite <guipleite@gmail.com>
2017-03-31 08:30:41 -0300

    Coloquie uma func

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 arquivo = open("inspermons.json" , "r")
 import random
 
+def main(arquivo,random)
 while True:
 	op = str(input("Deseja Passear ou Dormir? "))
 	opl = op.lower()
 	if opl == "passear" :
-		
+		ini = arquivo[random.int]
 		
 	if opl == "dormir" :
 		break
