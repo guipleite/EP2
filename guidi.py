5919b0f4f759adbeb918034d0ff16348f07e83c5
Guilherme Guidi <guilherme.guidi@icloud.com>
2017-04-07 11:22:23 -0300

    de novoo

--- guidi.py
+++ guidi.py
@@ -1,12 +1,12 @@
 def combate(ini,ipmon):
-   va = ipmon[1]	
-   vi = ini [1]
-   while True:
+    va = ipmon[1]	
+    vi = ini [1]
+    while True:
         va = va + ipmon[2] - ini[0]
         vi = vi + ini[2] - ipmon[0]
         if vi <= 0:
             print ("YOU WIN")
             return -1
         if va <= 0:  
-		     print("No ceu tem pão?")
+            print("No ceu tem pão?")
             break
