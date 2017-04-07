407a41f818c8dd48c1abaa77944d8d2d7c889e03
Guilherme Guidi <guilherme.guidi@icloud.com>
2017-04-07 10:55:11 -0300

    def conserto

--- guidi.py
+++ guidi.py
@@ -1,10 +1,10 @@
-def combate(ini,ipmon)
-while ini["vida"] >  ipmon"[vida"] or ipmon["vida"] > ini["vida"]:
-    ipmon["vida"] + ipmon[defesa] - ini["ataque"]
-    ini["vida"] + ipmon[defesa] - ipmon["ataque"]
-    if ini["vida"] > 0:
-        print ("Você perdeu a batalha!")
-    break
+def combate(ini,ipmon):
+    while ini["vida"] >  ipmon"[vida"] or ipmon["vida"] > ini["vida"]:
+        ipmon["vida"] + ipmon[defesa] - ini["ataque"]
+        ini["vida"] + ipmon[defesa] - ipmon["ataque"]
+        if ini["vida"] > 0:
+            print ("Você perdeu a batalha!")
+            break
           
        
         
