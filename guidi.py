0ab482aa51037b665a8041551ceb7780cfb19f76
Guilherme Guidi <guilherme.guidi@icloud.com>
2017-04-07 11:04:28 -0300

    vamo de novooo

--- guidi.py
+++ guidi.py
@@ -2,7 +2,7 @@ def combate(ini,ipmon):
     while ini[1] >  ipmon[1] or ipmon[1] > ini[1]:
         ipmon[1] + ipmon[2] - ini[0]
         ini[1] + ipmon[2] - ipmon[0]
-        if ini[1] > 0:
+        if ipmon[1] < 0:
             print ("Você perdeu a batalha!")
             break
           
